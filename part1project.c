68c0583c4b67bb6d27ca2118112d4dd3e3141e5c
AndreeaHoroba <161945683+AndreeaHoroba@users.noreply.github.com>
2024-03-28 13:20:12 +0200

    Add files via upload

---
+++ part1project.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<dirent.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc!=2)
+    {
+        perror("nu");
+        return -1;
+    }
+   DIR *dir;
+   dir=opendir(argv[1]);
+   struct dirent *readdir(dir);
+   if(dir!=NULL)
+   {
+    while(dirent!=NULL)
+    {
+        printf("%s",dirent->d_name);
+    }
+   }
+   closedir(dir);
+}
+
